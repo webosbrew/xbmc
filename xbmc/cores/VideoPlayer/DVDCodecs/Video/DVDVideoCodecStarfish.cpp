53e4cf0aca6fa478ed45500c663eff38fb23d810
Stephan Sundermann <stephansundermann@gmail.com>
2023-04-02 17:14:08 +0200

    webos: Fix play / pause failing sometimes

--- xbmc/cores/VideoPlayer/DVDCodecs/Video/DVDVideoCodecStarfish.cpp
+++ xbmc/cores/VideoPlayer/DVDCodecs/Video/DVDVideoCodecStarfish.cpp
@@ -411,18 +411,23 @@ void CDVDVideoCodecStarfish::SetCodecControl(int flags)
   {
     CLog::Log(LOGDEBUG, LOGVIDEO, "CDVDVideoCodecStarfish::{} {:x}->{:x}", __func__,
               m_codecControlFlags, flags);
-
-    if ((flags & DVD_CODEC_CTRL_DRAIN) && !(m_codecControlFlags & DVD_CODEC_CTRL_DRAIN))
-    {
-      m_starfishMediaAPI->Pause();
-    }
-
-    if (!(flags & DVD_CODEC_CTRL_DRAIN) && (m_codecControlFlags & DVD_CODEC_CTRL_DRAIN))
-    {
-      m_starfishMediaAPI->Play();
-    }
-
     m_codecControlFlags = flags;
+  }
+}
+
+void CDVDVideoCodecStarfish::SetSpeed(int iSpeed)
+{
+  switch (iSpeed)
+  {
+    case DVD_PLAYSPEED_NORMAL:
+      m_starfishMediaAPI->Play();
+      break;
+    case DVD_PLAYSPEED_PAUSE:
+      m_starfishMediaAPI->Pause();
+      break;
+    default:
+      CLog::Log(LOGWARNING, "CDVDVideoCodecStarfish::SetSpeed unknown playback speed");
+      break;
   }
 }
 
